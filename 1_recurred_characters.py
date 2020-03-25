d6df7805bd47510985b7b852698e12e226e77a06
Matteo Palazzi <43958921+palateo93@users.noreply.github.com>
2020-03-25 21:33:36 +0100

    Recurred characters excercises

---
+++ 1_recurred_characters.py
@@ -0,0 +1,42 @@
+def first_recurring_char(given_string):
+    counts = dict()
+    for char in given_string:
+        if char in counts:
+            return char
+        else:
+            counts[char] = 1
+    return None
+
+
+def first_non_recurring_char(given_string):
+    counts = dict()
+    min_index = len(given_string)
+    first_no_occ = None
+
+    for index, value in enumerate(given_string):
+        if value in counts:
+            counts[value] = (counts[value][0] + 1, counts[value][1])
+        else:
+            counts[value] = (1, index)
+    for (char, v) in counts.items():
+        if v[0] == 1 and v[1] < min_index:
+            min_index = v[1]
+            first_no_occ = char
+    return first_no_occ
+
+
+string = input("Enter the string: ")
+
+
+first_recurred = first_recurring_char(string)
+if first_recurred:
+    print("The first recurring character is: " + first_recurred)
+else:
+    print("There is no recurred characters")
+
+
+first_non_recurred = first_non_recurring_char(string)
+if first_non_recurred:
+    print("The first non recurring character is: " + first_non_recurred)
+else:
+    print("All characters are recurred")
